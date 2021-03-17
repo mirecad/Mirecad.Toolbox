5dc7600bb930961d99c12ba8abf896522bfcdf7c
Miroslav Adamec <miroslav.adamec@invia.cz>
2021-03-17 11:34:51 +0100

    Add RemoveDiacritics

---
+++ Mirecad.Toolbox.Tests/Extensions/StringExtensionsTests.cs
@@ -0,0 +1,29 @@
+﻿using FluentAssertions;
+using Mirecad.Toolbox.Extensions;
+using Xunit;
+
+namespace Mirecad.Toolbox.Tests.Extensions
+{
+    public class StringExtensionsTests
+    {
+        [Fact]
+        public void CanRemoveLowerCaseDiacritics()
+        {
+            var input = "ščťžý";
+
+            var result = input.RemoveDiacritics();
+
+            result.Should().Be("sctzy");
+        }
+
+        [Fact]
+        public void CanRemoveUpperCaseDiacritics()
+        {
+            var input = "ŠČŤŽÝ";
+
+            var result = input.RemoveDiacritics();
+
+            result.Should().Be("SCTZY");
+        }
+    }
+}
---
+++ Mirecad.Toolbox/Extensions/StringExtensions.cs
@@ -0,0 +1,27 @@
+﻿using System.Globalization;
+using System.Text;
+
+namespace Mirecad.Toolbox.Extensions
+{
+    public static class StringExtensions
+    {
+        /// <summary>
+        /// Removes diacritics from string.
+        /// </summary>
+        public static string RemoveDiacritics(this string input)
+        {
+            var normalized = input.Normalize(NormalizationForm.FormD);
+            var builder = new StringBuilder();
+
+            foreach (char ch in normalized)
+            {
+                if (CharUnicodeInfo.GetUnicodeCategory(ch) != UnicodeCategory.NonSpacingMark)
+                {
+                    builder.Append(ch);
+                }
+            }
+
+            return builder.ToString().Normalize(NormalizationForm.FormC);
+        }
+    }
+}
