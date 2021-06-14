996cc51a718144e4e9ecdf397ee5c9b6cb3e3939
Miroslav Adamec <miroslav.adamec@invia.cz>
2021-06-14 12:34:11 +0200

    Add ContainsIgnoreCare

---
+++ Mirecad.Toolbox.Tests/Extensions/EnumerableStringExtensions.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+using FluentAssertions;
+using Mirecad.Toolbox.Extensions;
+using Xunit;
+
+namespace Mirecad.Toolbox.Tests.Extensions
+{
+    public class EnumerableStringExtensions
+    {
+        [Fact]
+        public void ContainsIgnoreCareCanIgnoreCase()
+        {
+            var source = new List<string>(){"ONe", "TWO", "three", "FOur"};
+            var result = source.ContainsIgnoreCare("foUR");
+            result.Should().BeTrue();
+        }
+
+    }
+}
---
+++ Mirecad.Toolbox/Extensions/EnumerableStringExtensions.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace Mirecad.Toolbox.Extensions
+{
+    public static class EnumerableStringExtensions
+    {
+        public static bool ContainsIgnoreCare(this IEnumerable<string> collection, string target)
+        {
+            return collection.Any(x => x.ToLower() == target.ToLower());
+        }
+    }
+}
