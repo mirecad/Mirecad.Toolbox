0c299d6cf79550f1cd98d66b02a1e27a00298620
Miroslav Adamec <miroslav.adamec@invia.cz>
2022-01-13 13:40:25 +0100

    Add ContentLengthHandler

---
+++ Mirecad.Toolbox/Http/ContentLengthHandler.cs
@@ -0,0 +1,22 @@
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Invia.TextExtractor.Core.Infrastructure
+{
+    /// <summary>
+    /// Adds Content-Length header base of length of actual content.
+    /// </summary>
+    public class ContentLengthHandler : DelegatingHandler
+    {
+        protected override async Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, CancellationToken cancellationToken)
+        {
+            using (var stream = await request.Content.ReadAsStreamAsync())
+            {
+                request.Content.Headers.Add(@"Content-Length", stream.Length.ToString());
+            }
+            return await base.SendAsync(request, cancellationToken)
+                .ConfigureAwait(false);
+        }
+    }
+}
